dc9bf3474bdce3ece6952d0fba500b6bb0c61321
yangxiaotong <yangxiaotong@meituan.com>
2018-03-08 20:07:48 +0800

    angularjs learning

---
+++ js/route.js
@@ -0,0 +1,8 @@
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    .when('/', {template: 'hrhrhrhr'})
+    .when('/todos', {template: 'hdhddj'})
+    .when('/slider', {template: 'decfedvfdvf'})
+    .when('/help', {template: 'aaaaa'})
+}]);
